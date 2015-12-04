fbcce410ca9bfbb1bc6728579586fb8a64672a14
gdoriguetto <gustavo.doriguetto@odecee.com.au>
2015-12-04 15:00:42 +1100

    fixed topnav

--- website/ui.apps/src/main/content/jcr_root/apps/odecee/components/structure/navbar/navbar.js
+++ website/ui.apps/src/main/content/jcr_root/apps/odecee/components/structure/navbar/navbar.js
@@ -6,11 +6,29 @@ use(function () {
 
     while (it.hasNext()) {
         var page = it.next();
-		console.log(page.getProperties());
+
+		var subChannelsIterator = page.listChildren(new Packages.com.day.cq.wcm.api.PageFilter());
+        var subChannels =[];
+		var properties = page.getProperties();
+
+		if (page.properties.get('moreSection')) {
+		    var indexNumber = 0;
+            while(subChannelsIterator.hasNext() && page.properties.get('maxnav') > indexNumber) {
+                var subChannel = subChannelsIterator.next();
+                subChannels.push({
+                    page: subChannel,
+                    properties : subChannel.getProperties(),
+                    url: subChannel.getPath() + '.html'
+                })
+
+                indexNumber++;
+            }
+        }
         items.push({
             page: page,
-            properties : page.getProperties(),
-            url: page.getPath() + '.html'
+            properties : properties,
+            url: page.getPath() + '.html',
+            subChannels: subChannels
         });
     }
 
